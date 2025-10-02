bb92c6c66f924e52586638b005318afe36773aff
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-02 17:52:19 -0400

    Se creó getusecase

---
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/usecase/GetHuacalesUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.jendri_hidalgo_ap2_p1.domain.usecase
+
+import edu.ucne.jendri_hidalgo_ap2_p1.EntradaHucales
+import edu.ucne.jendri_hidalgo_ap2_p1.domain.repository.HuacalesRepository
+
+class GetHuacalesUseCase (
+    private val repository: HuacalesRepository
+) {
+    suspend operator fun invoke(id: Int): EntradaHucales? = repository.getHuacales(id)
+}
