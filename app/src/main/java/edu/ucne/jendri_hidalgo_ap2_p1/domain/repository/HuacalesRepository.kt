136aa06909cb83698d31589294ab7811dab66cec
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-06 23:05:36 -0400

    Se modifico el parametro.

--- app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/repository/HuacalesRepository.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/repository/HuacalesRepository.kt
@@ -1,12 +1,12 @@
 package edu.ucne.jendri_hidalgo_ap2_p1.domain.repository
 
-import edu.ucne.jendri_hidalgo_ap2_p1.domain.model.Huacales
+import edu.ucne.jendri_hidalgo_ap2_p1.domain.model.EntradaHucales
 import kotlinx.coroutines.flow.Flow
 
 interface HuacalesRepository {
-    fun observeHuacales(): Flow<List<Huacales>>
-    suspend fun getHuacales(id: Int): Huacales?
-    suspend fun upsert(huacales: Huacales): Int
+    fun observeHuacales(): Flow<List<EntradaHucales>>
+    suspend fun getHuacales(id: Int): EntradaHucales?
+    suspend fun upsert(huacales: EntradaHucales): Int
     suspend fun delete(id:Int)
     suspend fun existePorNombre(nombrecliente: String): Boolean
 }
