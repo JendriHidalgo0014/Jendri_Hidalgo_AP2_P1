6edabba22e60fd39eadbb1a0d9354135651574a3
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-06 23:07:00 -0400

    Se modifico la importacion

--- app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/usecase/GetHuacalesUseCase.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/usecase/GetHuacalesUseCase.kt
@@ -1,6 +1,7 @@
 package edu.ucne.jendri_hidalgo_ap2_p1.domain.usecase
 
-import edu.ucne.jendri_hidalgo_ap2_p1.EntradaHucales
+import edu.ucne.jendri_hidalgo_ap2_p1.domain.model.EntradaHucales
+
 import edu.ucne.jendri_hidalgo_ap2_p1.domain.repository.HuacalesRepository
 
 class GetHuacalesUseCase (
