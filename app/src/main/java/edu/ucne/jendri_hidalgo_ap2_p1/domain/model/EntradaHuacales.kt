c5084ecd3cfb610dd69567ebb1ad2f07b80031f4
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-06 23:07:27 -0400

    Se modifico la declaracion

--- app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/model/EntradaHuacales.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/domain/model/EntradaHuacales.kt
@@ -1,11 +1,11 @@
 package edu.ucne.jendri_hidalgo_ap2_p1.domain.model
 
 import kotlinx.serialization.Serializable
+import java.util.Date
 
-@Serializable
 data class EntradaHucales(
     val entradaId: Int = 0,
-    val fecha: String,
+    val fecha: Date,
     val nombrecliente: String,
     val cantidad: Int,
     val precio: Int
