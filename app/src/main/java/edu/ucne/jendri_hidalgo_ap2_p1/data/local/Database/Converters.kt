01d577664fec0f85bc75292969bde73a369d013d
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-06 23:09:40 -0400

    Se agrego converters para la fecha

---
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/data/local/Database/Converters.kt
@@ -0,0 +1,16 @@
+package edu.ucne.jendri_hidalgo_ap2_p1.data.local.Database
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
