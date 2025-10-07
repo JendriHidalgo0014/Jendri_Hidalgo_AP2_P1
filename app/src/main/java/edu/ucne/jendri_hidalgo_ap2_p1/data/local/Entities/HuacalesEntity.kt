1e1bbc1172cc1923afff3a4ba50008a658c7b5d2
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2025-10-06 23:06:15 -0400

    Se agrego el Entity

--- app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/data/local/Entities/HuacalesEntity.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/data/local/Entities/HuacalesEntity.kt
@@ -1,4 +1,16 @@
 package edu.ucne.jendri_hidalgo_ap2_p1.data.local.Entities
 
-class HuacalesEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+
+@Entity(tableName = "EntradaHuacales")
+data class HuacalesEntity(
+    @PrimaryKey(autoGenerate = true)
+    val entradaId: Int? = null,
+    val fecha: Date,
+    val nombrecliente: String = "",
+    val cantidad: Int = 0,
+    val precio: Int = 0
+)
