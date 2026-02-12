232ff287abad1ffc1eeac7b1a0cc7e00c8847258
Jendri Jose Hidalgo Rosario <jendri_hidalgo@ucne.edu.do>
2026-02-11 23:36:29 -0400

    Faltante.

--- app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/presentation/tareas/list/ListCervezaUiState.kt
+++ app/src/main/java/edu/ucne/jendri_hidalgo_ap2_p1/presentation/tareas/list/ListCervezaUiState.kt
@@ -1,7 +1,6 @@
 package edu.ucne.jendri_hidalgo_ap2_p1.presentation.tareas.list
 
 import edu.ucne.jendri_hidalgo_ap2_p1.domain.model.Cervezas
-
 data class ListCervezaUiState(
     val isLoading: Boolean = false,
     val cervezas: List<Cervezas> = emptyList(),
